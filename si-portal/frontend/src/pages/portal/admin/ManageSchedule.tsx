bbe8ecc88f80d4a435b17bac77eabd21eb0e59fa
john <sgcho@bistelligence.ai>
2025-01-20 09:38:27 +0900

    [스케줄관리]추가 삭제 업데이트에 따른 job 상태변경 중간 저장

--- si-portal/frontend/src/pages/portal/admin/ManageSchedule.tsx
+++ si-portal/frontend/src/pages/portal/admin/ManageSchedule.tsx
@@ -44,6 +44,7 @@ const ManageSchedule: React.FC = () => {
   
     const inputRef = useRef<HTMLInputElement>(null);
     const gridRef = useRef<AgGridWrapperHandle>(null);
+    let scheduleData: any[] = [];
   
   
     const [deletedRows, setDeletedRows] = useState([]); // 삭제된 행 관리
@@ -68,6 +69,7 @@ const ManageSchedule: React.FC = () => {
         console.log('gridRef.current', gridRef.current)
           if (gridRef.current) {
             gridRef.current.setRowData(res.data); // 데이터를 AgGridWrapper에 설정
+            scheduleData = JSON.parse(JSON.stringify(res.data));
           }
           comAPIContext.hideProgressBar();
           comAPIContext.showToast('조회가 완료됐습니다.','dark');
@@ -81,38 +83,61 @@ const ManageSchedule: React.FC = () => {
       });
     };
   
-    const handleSave = async (lists: { deleteList: any[]; updateList: any[] }) => {
+    const handleSave = async (lists: { deleteList: any[]; updateList: any[]; createList: any[] }) => {
         if (!gridRef.current) return;
 
-        if (lists.deleteList.length === 0 && lists.updateList.length === 0) {
+        if (lists.deleteList.length === 0 && lists.updateList.length === 0 && lists.createList.length === 0) {
           comAPIContext.showToast('저장할 데이터가 없습니다.', 'dark');
           return;
         }
-    
-    
+        
         try {
-          comAPIContext.showProgressBar();
-          console.log('1.update 행들:', lists);
-          console.log('2.delete 행들:', lists);
-    
-          // 전송 데이터 구성
-          const payload = {
-            updateList: lists.updateList,
-            deleteList: lists.deleteList,
-          };
-    
-          await axios.post('http://localhost:8080/admin/api/update-schedule', payload, {
-            headers: { Authorization: `Bearer ${state.authToken}` },
-          });
-    
-          comAPIContext.showToast('저장되었습니다.', 'success');
-          handleSearch(); // 저장 후 최신 데이터 조회
+            comAPIContext.showProgressBar();
+            console.log('1.update 행들:', lists.updateList);
+            console.log('2.delete 행들:', lists.deleteList);
+            console.log('3.create 행들:', lists.createList);
+
+            let realUpdateList : any = []
+            if(lists.updateList?.length > 0) {
+                for(const obj of lists.updateList){
+                    const temp : any = scheduleData.find(e=>e.jobName === obj.jobName && e.groupName === obj.groupName && e.triggerKey === obj.triggerKey && e.className === obj.className)
+                    // console.log('temp:', temp);
+                    // console.log('temp?.cronTab:', temp?.cronTab);
+                    // console.log('obj.cronTab:', obj.cronTab);
+                    // console.log('temp?.status:', temp?.status);
+                    // console.log('obj.status:', obj.status);
+
+                    if(temp != undefined && (temp.cronTab != obj.cronTab || temp.status != obj.status)){
+                        if(temp.cronTab != obj.cronTab){
+                            obj.changeCron = 'Y'
+                        }
+                        if(temp.status != obj.status){
+                            obj.changeStatus = 'Y'
+                        }
+                        realUpdateList.push(obj)
+                    }
+                }
+            }
+            // console.log('realUpdateList:', realUpdateList);
+            // 전송 데이터 구성
+            const payload = {
+                updateList: realUpdateList,
+                deleteList: lists.deleteList,
+                createList: lists.createList,
+            };
+    console.log('realUpdateList:', realUpdateList);
+            await axios.post('http://localhost:8080/admin/api/update-schedule', payload, {
+                headers: { Authorization: `Bearer ${state.authToken}` },
+            });
+        
+            comAPIContext.showToast('저장되었습니다.', 'success');
+            handleSearch(); // 저장 후 최신 데이터 조회
         } catch (err) {
-          console.error('Error saving data:', err);
-          comAPIContext.showToast('저장 중 오류가 발생했습니다.', 'danger');
-          handleSearch();
+            console.error('Error saving data:', err);
+            comAPIContext.showToast('저장 중 오류가 발생했습니다.', 'danger');
+            handleSearch();
         } finally {
-          comAPIContext.hideProgressBar();
+            comAPIContext.hideProgressBar();
         }
     };
 
@@ -125,12 +150,21 @@ const ManageSchedule: React.FC = () => {
             const uneditableCell = ['jobName', 'groupName', 'triggerKey', 'className']
             if (uneditableCell.includes(event.colDef.field) && oldValue !== undefined && oldValue !== newValue) {
                 // 편집 취소하고 원래 값으로 되돌리기
-                event.node.setDataValue(event.column, oldValue);
+                event.node.setDataValue(event.column, oldValue, false);//suppressEvent: true로 설정하는 방법을 사용하여 이벤트가 재발생하지 않도록 설정
                 event.api.stopEditing(false); // 편집 취소
                 comAPIContext.showToast('해당 컬럼은 변경할 수 없습니다.', 'danger');
             } 
         }, 0);  // 타이밍을 조금 늦춰서 호출
     };
+
+    const onCellEditingStarted = (event: any) => {
+    }
+
+    const onCellValueChanged = (event: any) => {
+
+        console.log('Cell changed', event);
+    }
+
     
     return (
         <Container fluid>
@@ -170,6 +204,8 @@ const ManageSchedule: React.FC = () => {
                   enableCheckbox={true}
                   onSave={handleSave} // 저장 버튼 동작
                   onCellEditingStopped={onCellEditingStopped} // 셀편집마침 이벤트 핸들러 등록
+                  onCellEditingStarted={onCellEditingStarted} // 셀편집시작
+                  onCellValueChanged={onCellValueChanged} // 셀변경
               />
             </Col>
           </Row>
