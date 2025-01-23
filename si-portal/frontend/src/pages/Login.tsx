747a6cd3abd62614715e0252072d752c33d36f9e
flora <selee@bistelligence.ai>
2025-01-23 10:50:44 +0900

    style: publishing login page in progress

--- si-portal/frontend/src/pages/Login.tsx
+++ si-portal/frontend/src/pages/Login.tsx
@@ -57,33 +57,38 @@ const Login = () => {
   };
 
   return (
-    <Container className="mt-5" style={{ maxWidth: "400px" }}>
-      <h2 className="text-center">Login</h2>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicEmail" className="mb-3">
-          <Form.Label>ID</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="ID를 입력하세요"
-            ref={userIdRef}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword" className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="비밀번호를 입력하세요"
-            ref={passwordRef}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" className="w-100">
-          Login
-        </Button>
-        <Button variant="link" className="w-100">
-          Don’t have an account?
-        </Button>
-      </Form>
-    </Container>
+    <div className={styles.start}>
+      <Container className={styles.container}>
+        <div className={styles.title_area}>
+          <img alt="기업 로고" src="" />
+          <p className={styles.explain}>Sign in to your account to continue</p>
+        </div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail" className="mb-3">
+            <Form.Label>ID</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="ID를 입력하세요"
+              ref={userIdRef}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword" className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+              ref={passwordRef}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="w-100">
+            Login
+          </Button>
+          <Button variant="link" className="w-100">
+            Don’t have an account?
+          </Button>
+        </Form>
+      </Container>
+    </div>
   );
 };
 
