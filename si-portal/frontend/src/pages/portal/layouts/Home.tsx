36cdfd96ffa8dc14929787a88e1def45a3855568
Six-canon captin <37443674+suersehii1@users.noreply.github.com>
2025-02-03 14:58:12 +0900

    오라클도 지원하게  수정함

--- si-portal/frontend/src/pages/portal/layouts/Home.tsx
+++ si-portal/frontend/src/pages/portal/layouts/Home.tsx
@@ -11,17 +11,22 @@ const Home: React.FC = () => {
         </Col>
       </Row>
       <Row className="mt-4">
-        <Col>
-          <h2>되는 기능 List </h2>
-            <ul>
-                <li>1. Progress bar 지원됨</li>
-                <li>2. Toast message 지원됨</li>
-                <li>3. Mouse move 및 key down 이 10분간 없을시 자동 logout 기능 지원됨.</li>
-                <li>4. Button 클릭시 session 체크 기능 지원함 (Loader.tsx가 기능 지원함)</li>
-                <li>5. Grid는 Ag grid 사용함. ( Ag grid 사용시 AgGridWrapper 를 사용하면됨. 무료이므로 EXCEL 관련 지원 안함)</li>
-                <li>6. 메뉴의 depth 는 3 LEVLE 까지 가능함.</li>
-            </ul>
-        </Col>
+          <Col>
+              <h2>지원하는 DB </h2>
+              <ul>
+                  <li>1. Oralce</li>
+                  <li>2. Postgresql</li>
+              </ul>
+              <h2>지원하는 기능 List </h2>
+              <ul>
+                  <li>1. Progress bar 지원됨</li>
+                  <li>2. Toast message 지원됨</li>
+                  <li>3. Mouse move 및 key down 이 10분간 없을시 자동 logout 기능 지원됨.</li>
+                  <li>4. Button 클릭시 session 체크 기능 지원함 (Loader.tsx가 기능 지원함)</li>
+                  <li>5. Grid는 Ag grid 사용함. ( Ag grid 사용시 AgGridWrapper 를 사용하면됨. 무료이므로 EXCEL 관련 지원 안함)</li>
+                  <li>6. 메뉴의 depth 는 3 LEVLE 까지 가능함.</li>
+              </ul>
+          </Col>
       </Row>
         <Row className="mt-4">
             <Col>
