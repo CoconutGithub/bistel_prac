18575ddcadb19b1b87c8956135d2aba460d4b57f
Six-canon captin <37443674+suersehii1@users.noreply.github.com>
2025-02-18 15:58:02 +0900

    이력서 작성

--- si-portal/server_JPA/src/main/java/com/siportal/portal/domain/Resume.java
+++ si-portal/server_JPA/src/main/java/com/siportal/portal/domain/Resume.java
@@ -7,7 +7,6 @@ import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
-import org.postgresql.util.PGobject;
 
 import java.time.LocalDateTime;
 import java.util.List;
