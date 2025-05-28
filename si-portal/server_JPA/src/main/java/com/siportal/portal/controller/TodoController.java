36c568ce0f1a43221785085726e1add04f119076
yoon yang <ysyang@bistelligence.ai>
2025-05-28 17:17:41 +0900

    fix: TodoController 오류 수정

--- si-portal/server_JPA/src/main/java/com/siportal/portal/controller/TodoController.java
+++ si-portal/server_JPA/src/main/java/com/siportal/portal/controller/TodoController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,8 @@ import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController("api/todo")
+@RestController
+@RequestMapping("/api/todo")
 @RequiredArgsConstructor
 public class TodoController {
 
