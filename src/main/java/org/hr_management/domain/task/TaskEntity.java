808477ca868154f60cc34c9388c874112348e0cc
Coconut <khkoh5932@gmail.com>
2025-07-23 13:29:30 +0900

    feat: m_salary, status, task Entity
    월급, 상태, 업무 엔티티 생성

---
+++ src/main/java/org/hr_management/domain/task/TaskEntity.java
@@ -0,0 +1,52 @@
+package org.hr_management.domain.task;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hr_management.domain.employee.db.EmployeeEntity;
+import org.hr_management.domain.status.StatusEntity;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "TASK")
+@Getter
+@Setter
+public class TaskEntity {
+
+    @Id
+    @Column(name = "TASK_ID")
+    private Long taskId;
+
+    @Column(name = "TASK_TITLE", length = 100, nullable = false)
+    private String taskTitle;
+
+    @Column(name = "START_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    @Column(name = "DUE_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date dueDate;
+
+    @JoinColumn(name = "STATUS_CODE")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private StatusEntity statusCode;
+
+    @Column(name = "PRIORITY")
+    private Integer priority;
+
+    @Lob
+    @Column(name = "TASK_DESCRIPTION")
+    private String taskDescription;
+
+    @Column(name = "ASSIGNED_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date assignedDate;
+
+    @JoinColumn(name = "EMP_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private EmployeeEntity empId;
+
+
+}
