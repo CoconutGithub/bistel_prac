acfbd5f368113bd99057a34a8334d79500d010d0
Coconut <khkoh5932@gmail.com>
2025-07-30 17:27:36 +0900

    refactor: StatusRepo삭제

--- src/main/java/org/hr_management/domain/status/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-//package org.hr_management.domain.status;
-//
-//import org.hr_management.domain.status.db.StatusEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface StatusRepository extends JpaRepository<StatusEntity, String> {
-//}
