d55cd1f7ca53a7e39add95d7e4fd779c815113bd
Coconut <khkoh5932@gmail.com>
2025-08-01 17:01:11 +0900

    feat: 로그인, 메뉴 라우팅

--- hr_management/frontend/src/App.tsx
+++ hr_management/frontend/src/App.tsx
@@ -7,18 +7,21 @@ import TaskList from './pages/TaskList';
 import TaskRegisterForm from './pages/TaskRegisterForm';
 import SalaryList from "./pages/SalaryList";
 import SalaryPaymentForm from "./pages/SalaryPaymentForm";
+import Login from "./pages/Login";
+import Menu from "./pages/Menu";
 
 const App: React.FC = () => (
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{height: '100vh', width: '100%'}}>
         <Routes>
-            <Route path="/employee" element={<EmployeeList />} />
-            <Route path="/employee/register" element={<EmployeeRegister />} />
-            <Route path="/task" element={<TaskList />} />
-            <Route path="/task/register" element={<TaskRegisterForm />} />
-            <Route path="/salary" element={<SalaryList />} />
-            <Route path="/salary/payment/:empId" element={<SalaryPaymentForm />} />
-            <Route path="/salary/payment/" element={<SalaryPaymentForm />} />
-            <Route path="*" element={<EmployeeList />} />
+            <Route path="/employee" element={<EmployeeList/>}/>
+            <Route path="/employee/register" element={<EmployeeRegister/>}/>
+            <Route path="/task" element={<TaskList/>}/>
+            <Route path="/task/register" element={<TaskRegisterForm/>}/>
+            <Route path="/salary" element={<SalaryList/>}/>
+            <Route path="/salary/payment/:empId" element={<SalaryPaymentForm/>}/>
+            <Route path="/salary/payment/" element={<SalaryPaymentForm/>}/>
+            <Route path="/menu" element={<Menu />} />
+            <Route path="*" element={<Login />} />
         </Routes>
     </div>
 );
