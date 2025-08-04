a0058e6456b806ddb62f0cda9be68ce470eb9436
Coconut <khkoh5932@gmail.com>
2025-08-04 13:35:54 +0900

    feat: jwt토큰 생성, 로그인 필터 추가

--- hr_management/src/main/java/org/hr_management/config/SecurityConfig.java
+++ hr_management/src/main/java/org/hr_management/config/SecurityConfig.java
@@ -1,38 +1,57 @@
 package org.hr_management.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.hr_management.domain.jwt.JwtFilter;
+import org.hr_management.domain.jwt.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtUtil jwtUtil;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // CSRF
-        http.csrf((it) -> it.disable());
-
-        // CORS
-//        http.cors(cors -> cors.configurationSource(request -> {
-//            CorsConfiguration configuration = new CorsConfiguration();
-//            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
-//            configuration.setAllowedHeaders(Arrays.asList("*"));
-//            configuration.setAllowCredentials(true);
-//            return configuration;
-//        }));
-
-        http.authorizeHttpRequests((authorizeRequests) ->
-                authorizeRequests
-                        .requestMatchers("/employee/login","/").permitAll()
-//                        .anyRequest().authenticated()
-                        .anyRequest().permitAll()
-        );
-
+//        http
+//                .cors((cors) -> cors
+//                        .configurationSource(new CorsConfigurationSource() {
+//                            @Override
+//                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//                                CorsConfiguration configuration = new CorsConfiguration();
+//
+//                                configuration.setAllowedMethods(Collections.singletonList("*"));
+//                                configuration.setAllowCredentials(true);
+//                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+//                                configuration.setMaxAge(3600L);
+//
+//                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//
+//                                return configuration;
+//                            }
+//                        }));
+        http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic((auth -> auth.disable()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/employee/login").permitAll()
+                                .anyRequest().authenticated()
+//                                .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
---
+++ hr_management/src/main/java/org/hr_management/domain/jwt/JwtUtil.java
@@ -0,0 +1,37 @@
+package org.hr_management.domain.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private SecretKey secretKey;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public Integer getEmpId(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("empId", Integer.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(Integer empId, Long expiredMs) {
+        return Jwts.builder()
+                .claim("empId", empId)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
