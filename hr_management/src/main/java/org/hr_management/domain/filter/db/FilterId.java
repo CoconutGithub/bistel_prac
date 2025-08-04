c1e3a6e26281cb509f8753a956152190ea257106
Coconut <khkoh5932@gmail.com>
2025-08-04 13:38:31 +0900

    feat: 사용자화 필터 Entity

---
+++ hr_management/src/main/java/org/hr_management/domain/filter/db/FilterId.java
@@ -0,0 +1,19 @@
+package org.hr_management.domain.filter.db;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class FilterId implements Serializable {
+    private Integer empId;
+    private String tableName;
+    private String filterName;
+    private String filterType;
+}
