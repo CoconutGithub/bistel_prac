78370d8819bd30347698f65c70363a070c0590d8
Coconut <khkoh5932@gmail.com>
2025-08-14 13:57:27 +0900

    feat: first commit in semiconductor porj

---
+++ semiconductor/src/main/java/com/prac/semiconductor/SemiconductorApplication.java
@@ -0,0 +1,13 @@
+package com.prac.semiconductor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiconductorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiconductorApplication.class, args);
+    }
+
+}
