8ab196e6794f3d451c35b73ff665ee322c4874c3
Coconut <khkoh5932@gmail.com>
2025-08-19 13:04:29 +0900

    feat: Line에서 속한 모든 내용 한번에 조회

--- semiconductor/src/main/java/com/prac/semiconductor/Repository/LineRepository.java
+++ semiconductor/src/main/java/com/prac/semiconductor/Repository/LineRepository.java
@@ -2,6 +2,20 @@ package com.prac.semiconductor.Repository;
 
 import com.prac.semiconductor.Domain.Line;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LineRepository extends JpaRepository<Line, Integer> {
+    /**
+     * 모든 Line과 관련된 Process, Equipment, SetValue, Parameter를
+     * 한 번의 쿼리로 모두 조회합니다 (N+1 문제 해결).
+     * `DISTINCT`를 사용하여 Line이 중복 조회되는 것을 방지합니다.
+     */
+    @Query("SELECT DISTINCT l FROM Line l " +
+            "LEFT JOIN FETCH l.processes p " +
+            "LEFT JOIN FETCH p.equipments e " +
+            "LEFT JOIN FETCH e.setValues sv " +
+            "LEFT JOIN FETCH sv.parameter")
+    List<Line> findAllWithDetails();
 }
