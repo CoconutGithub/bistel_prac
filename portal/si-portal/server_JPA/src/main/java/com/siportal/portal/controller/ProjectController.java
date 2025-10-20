893c0680e6076294215a54a3931a115aa794ed89
Coconut <khkoh5932@gmail.com>
2025-10-20 16:00:51 +0900

    feat: project Controller

---
+++ portal/si-portal/server_JPA/src/main/java/com/siportal/portal/controller/ProjectController.java
@@ -0,0 +1,29 @@
+package com.siportal.portal.controller;
+
+import com.siportal.portal.domain.Project;
+import com.siportal.portal.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://192.168.7.37:9090", "http://localhost:9090"})
+@RequestMapping("/project")
+@Transactional
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @GetMapping("/list")
+    public ResponseEntity<?> listProject(){
+        return projectService.allProject();
+    }
+
+//    @GetMapping("/detail/{project_id}")
+//    public ResponseEntity<?> detailProject(@PathVariable("project_id") Long project_id){
+//
+//    }
+}
