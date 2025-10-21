d64b7341ed8db338dda42715652d24c108bfd17c
Coconut <khkoh5932@gmail.com>
2025-10-21 09:35:05 +0900

    feat: ProjectDetailDTO

---
+++ portal/si-portal/server_JPA/src/main/java/com/siportal/portal/dto/ProjectDetailDTO.java
@@ -0,0 +1,26 @@
+package com.siportal.portal.dto;
+
+import com.siportal.portal.domain.ProjectHumanResource;
+import com.siportal.portal.domain.ProjectProgressDetail;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class ProjectDetailDTO {
+    private Long projectId;
+    private String projectName;
+    private String projectCode;
+    private String projectDescription;
+    private String projectStatus;
+    private String step;
+    private String pmId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Integer overallProgress;
+    private List<ProjectProgressDetail>  projectProgressDetails;
+    private List<ProjectHumanResource> projectHumanResources;
+}
