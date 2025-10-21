a12afb32f9ea2ca955edc0e959b0b3fced8f61a7
Coconut <khkoh5932@gmail.com>
2025-10-21 15:32:33 +0900

    feat: update project controller

--- portal/si-portal/server_JPA/src/main/java/com/siportal/portal/controller/ProjectController.java
+++ portal/si-portal/server_JPA/src/main/java/com/siportal/portal/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package com.siportal.portal.controller;
 
 import com.siportal.portal.domain.Project;
+import com.siportal.portal.dto.ProjectDetailDTO;
 import com.siportal.portal.dto.ProjectSaveDTO;
 import com.siportal.portal.service.ProjectService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -18,14 +21,15 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @GetMapping("/list")
-    public ResponseEntity<?> listProject(){
+    public ResponseEntity<?> listProject() {
         return projectService.allProject();
     }
 
     @GetMapping("/detail/{project_id}")
-    public ResponseEntity<?> detailProject(@PathVariable("project_id") Long projectId){
+    public ResponseEntity<?> detailProject(@PathVariable("project_id") Long projectId) {
         return projectService.detailProject(projectId);
     }
+
     @PostMapping("/save")
     public ResponseEntity<?> saveProjects(@RequestBody ProjectSaveDTO saveRequest) {
         try {
@@ -36,6 +40,22 @@ public class ProjectController {
             return ResponseEntity
                     .internalServerError()
                     .body("Error saving projects: " + e.getMessage());
+        }
+    }
+
+    @PutMapping("/update/{project_id}")
+    public ResponseEntity<?> updateProject(@RequestBody ProjectDetailDTO projectDTO, @PathVariable("project_id") Long projectId) {
+        try {
+            Project updatedProject = projectService.updateProject(projectDTO, projectId);
+            // 성공 시 업데이트된 객체 정보와 함께 200 OK 응답
+            return ResponseEntity.ok().body(updatedProject);
+        } catch (EntityNotFoundException e) {
+            // 프로젝트를 찾지 못했을 경우 404 Not Found 응답
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body("Error updating project: " + e.getMessage());
         }
     }
 }
