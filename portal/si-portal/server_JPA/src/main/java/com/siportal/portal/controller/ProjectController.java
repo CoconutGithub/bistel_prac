c947af57a405cae24dc4513ec4d3ab50fce14da2
Coconut <khkoh5932@gmail.com>
2025-10-23 09:42:29 +0900

    feat: C/U/D progress detail controller

--- portal/si-portal/server_JPA/src/main/java/com/siportal/portal/controller/ProjectController.java
+++ portal/si-portal/server_JPA/src/main/java/com/siportal/portal/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.siportal.portal.controller;
 
 import com.siportal.portal.domain.Project;
+import com.siportal.portal.dto.ProgressSaveDTO;
 import com.siportal.portal.dto.ProjectDetailDTO;
 import com.siportal.portal.dto.ProjectSaveDTO;
 import com.siportal.portal.service.ProjectService;
@@ -79,6 +80,27 @@ public class ProjectController {
             return ResponseEntity
                     .internalServerError()
                     .body("Error deleting project: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/progress/save/{project_id}")
+    public ResponseEntity<?> saveProgressDetails(@RequestBody ProgressSaveDTO progressSaveDTO, @PathVariable("project_id") Long projectId) {
+        try {
+            projectService.saveProgressDetails(projectId,progressSaveDTO);
+            return ResponseEntity.ok().body("진행 상세가 저장되었습니다.");
+
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (AccessDeniedException e) {
+            // PM 권한이 없는 경우
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        } catch (ProjectService.WeightValidationException e) {
+            // 상세 항목들의 가중치 합이 100이 안되는 경우
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body("Error saving progress details: " + e.getMessage());
         }
     }
 
